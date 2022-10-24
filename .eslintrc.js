7d9c81a32de9c4ee1c9918026f525f09964c8ad3
HaruToy <twitty3420@gmail.com>
2022-10-24 12:40:57 +0900

    #1 sprint - front init

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:vue/recommended',
+    'plugin:vue/essential',
+    '@vue/airbnb',
+    'prettier'
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+  },
+  overrides: [
+    {
+      files: [
+        '**/__tests__/*.{j,t}s?(x)',
+        '**/tests/unit/**/*.spec.{j,t}s?(x)',
+      ],
+      env: {
+        node: true,
+        browser: true,
+        jest: true,
+      },
+    },
+  ],
+};
---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports={
+  semi:true,
+  tailingComma:'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 2,
+  endOfLine: "auto",
+  useTabs: false,
+};
